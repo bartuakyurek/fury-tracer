e07fa7dbd7bb2cea4d481753add64eeee040bf4b
bartuakyurek <77360680+bartuakyurek@users.noreply.github.com>
2025-10-30 17:16:07 +0300

    Move ray triangle intersection to geometry
    To declutter shapes.rs but perhaps there could be an intersection
    module (and a crate to manage all the shapes and geometry)

--- src/geometry.rs
+++ src/geometry.rs
@@ -15,6 +15,7 @@
 use std::{ops::Index};
 use crate::shapes::{Triangle};
 use crate::numeric::{Float, Vector3, approx_zero};
+use crate::{ray::Ray, interval::Interval, dataforms::VertexData};
 
 pub fn get_tri_normal(v1: &Vector3, v2: &Vector3, v3: &Vector3) -> Vector3{
     // WARNING: Assumes triangle indices are given in counter clockwise order 
@@ -30,6 +31,144 @@ pub fn get_tri_normal(v1: &Vector3, v2: &Vector3, v3: &Vector3) -> Vector3{
     
     debug_assert!(approx_zero(normal.length() - 1.0));
     normal
+}
+
+
+//fn get_beta_gamma_t(a: Vector3, b: Vector3, c: Vector3, o: Vector3, d: Vector3) -> (Float, Float, Float) { 
+//    // Helper function for computations in Slides 01_B, p.30
+//    // a, b, c are triangle corners
+//    // o, d are ray's origin and direction r(t) = o + d * t
+//    //
+//    // TODO: reduce verbosity and unnecessary operations
+//    // I've written in naive way for the beginning
+//    let (ax, ay, az) = (a[0], a[1], a[2]);
+//    let (bx, by, bz) = (b[0], b[1], b[2]);
+//    let (cx, cy, cz) = (c[0], c[1], c[2]);
+//    let (ox, oy, oz) = (o[0], o[1], o[2]);
+//    let (dx, dy, dz) = (d[0], d[1], d[2]);
+//
+//    // Construct A
+//    let A_x = Vector3::new(ax - bx, ay - by, az - bz);
+//    let A_y = Vector3::new(ax - cx, ay - cy, az - cz);
+//    let A_z = Vector3::new(dx, dy, dz);
+//    let A = Matrix3::from_cols(A_x, A_y, A_z);
+//    let A_determinant = A.determinant();
+//
+//    // Construct beta 
+//    let beta_x = Vector3::new(ax - ox, ay - oy, az - oz);
+//    let beta_y = Vector3::new(ax - cx, ay - cy, az - cz);
+//    let beta_z = Vector3::new(dx, dy, dx);
+//    let beta_matrix = Matrix3::from_cols(beta_x, beta_y, beta_z);
+//    let beta = beta_matrix.determinant() / A_determinant;
+//
+//    // Construct gamma
+//    let gamma_x = Vector3::new(ax - bx, ay - by, az - bz);
+//    let gamma_y = Vector3::new(ax - ox, ay - oy, az - oz);
+//    let gamma_z = Vector3::new(dx, dy, dz);
+//    let gamma_matrix = Matrix3::from_cols(gamma_x, gamma_y, gamma_z);
+//    let gamma = gamma_matrix.determinant() / A_determinant;
+//
+//    let t_x = Vector3::new(ax - bx, ay - by, az - bz);
+//    let t_y = Vector3::new(ax - cx, ay - cy, az - cz);
+//    let t_z = Vector3::new(ax - ox, ay - oy, az - oz);
+//    let t_matrix = Matrix3::from_cols(t_x, t_y, t_z);
+//    let t = t_matrix.determinant() / A_determinant;
+//
+//    (beta, gamma, t)
+//}
+
+//fn lengthy_but_simple_intersection(ray: &Ray, t_interval: &Interval, tri_indices: [usize; 3], verts: &VertexData) -> Option<(Vector3, Float)> {
+//    // Slides 01_B, p.14
+//    //
+//    //  n    a  
+//    //   \  / \
+//    //     /   \
+//    //   b ----- c
+//    let [a, b, c] = tri_indices.map(|i| verts[i]);
+//    let (beta, gamma, t) = get_beta_gamma_t(a, b, c, ray.origin, ray.direction);
+//
+//    // Conditions at p.32
+//    if !t_interval.contains(t) {
+//        return None;
+//    }
+//    if (beta + gamma) > 1. {
+//        return None;
+//    }
+//    if (0. > beta) || (0. > gamma) {
+//        return None;
+//    }
+//
+//    // Construct p from barycentric coords
+//    let p = ray.at(t);
+//    //let p = a + (beta * (b - a)) + (gamma * (c - a)); // p.27
+//    //assert!(approx_zero((p-pt).norm())); TODO: Why does it fail?
+//
+//    // Check for edge BA 
+//    let edge_ba = a - b;
+//    let edge_bc = c - b;
+//    let n = (edge_bc).cross(edge_ba); // vc in p.16
+//    let vp = (p - b).cross(edge_ba); // TODO: we can use the same vp for other checks, right?
+//    if vp.dot(n) <= 0.0 {
+//        return None;
+//    }
+//
+//    // Check for AC
+//    let edge_ca = a - c;
+//    let edge_ac = c - a;
+//    let vb = (edge_bc).cross(edge_ac);
+//    if vb.dot(n) <= 0.0 {
+//        return None;
+//    }
+//
+//    // Check for CB
+//    let edge_cb = b - c;
+//    let va = (edge_ca).cross(edge_cb);
+//    if va.dot(n) <= 0.0 {
+//        return None;
+//    }
+//
+//    Some((p, t))
+//}
+
+pub fn moller_trumbore_intersection(ray: &Ray, t_interval: &Interval, tri_indices: [usize; 3], verts: &VertexData) -> Option<(Float, Float, Float)> {
+    // Based on Möller-Trumbore algorithm
+    //
+    //     a (pivot)
+    //    / \
+    //  b  -  c
+    // 
+    // WARNING: Assumes given interval has incorporated relevant epsilon e.g.
+    // instead of [0.0, inf], [0.0001, inf] is given otherwise there might be
+    // floating point errors.
+    // TODO: Is there something wrong in this function?
+    let tri_coords = tri_indices.map(|i| verts[i]);
+    let [tri_pivot, tri_left, tri_right] = tri_coords;        
+    let edge_ab = tri_left - tri_pivot;
+    let edge_ac = tri_right - tri_pivot;
+    // Scalar triple product https://youtu.be/fK1RPmF_zjQ
+    debug_assert!(ray.direction.is_normalized());
+    let perp = ray.direction.cross(edge_ac);
+    let determinant: Float = perp.dot(edge_ab);
+    if (determinant > -t_interval.min) && (determinant < t_interval.min) {
+        return None;
+    }
+    let inverse_determinant = 1.0 as Float / determinant;
+    let dist = ray.origin - tri_pivot;
+    let barycentric_u = dist.dot(perp) * inverse_determinant;
+    if (barycentric_u < 0.0) || (barycentric_u > 1.0) {
+        return None;
+    }
+    let another_perp = dist.cross(edge_ab);
+    let barycentric_v = ray.direction.dot(another_perp) * inverse_determinant;
+    if (barycentric_v < 0.0) || ((barycentric_u + barycentric_v) > 1.0) {
+        return None;
+    }
+    // Get ray t
+    let t = edge_ac.dot(another_perp) * inverse_determinant;
+    if !t_interval.contains(t) {
+        return None;
+    }
+    Some((barycentric_u, barycentric_v, t))
 }
 
 
@@ -61,20 +200,6 @@ impl CoordLike {
         }
     }
 }
-
-
-//impl Index<usize> for CoordLike {
-//    // To access data through indexing like
-//    // let some_field = DataField::default()
-//    // some_field[i] = ...
-//    // instead of some_field._data[i]
-//    type Output = [Float; 3];
-//    fn index(&self, index: usize) -> &Self::Output {
-//        [self.xs[index], self.ys[index], self.xs[index]] // doesnt work because it's temporary value
-//    }
-//}
-
-
 
 impl StructofArrays for CoordLike {
     type Item = Vector3;
--- src/shapes.rs
+++ src/shapes.rs
@@ -14,7 +14,7 @@ use bevy_math::NormedVectorSpace;
 use serde::{Deserialize};
 use smart_default::SmartDefault;
 use tracing::{info, error};
-use crate::geometry::get_tri_normal;
+use crate::geometry::{get_tri_normal, moller_trumbore_intersection};
 use crate::json_parser::*;
 use crate::interval::{Interval};
 use crate::dataforms::{VertexData};
@@ -128,193 +128,39 @@ impl PrimitiveShape for Triangle {
         // this is not handled yet and our assumption is VertexData is the only source of vertices, every
         // shape refers to this data for their coordinates. 
         
-        //info!("todo: convert u,v barycentric to coords");
         let verts = &vertex_cache.vertex_data;
         if let Some((u, v, t)) = moller_trumbore_intersection(ray, t_interval, self.indices, verts) {
             
             let p = ray.at(t); // Construct hit point p // TODO: would it be faster to use barycentric u,v here? 
             let tri_normal = {
-                //if let Some(cache) = cache {
-                    if self.is_smooth {
-                        let v1_n = vertex_cache.vertex_normals[self.indices[0]];
-                        let v2_n = vertex_cache.vertex_normals[self.indices[1]];
-                        let v3_n = vertex_cache.vertex_normals[self.indices[2]];
-                        let w = 1. - u - v;
-                        (v1_n * w + v2_n * u + v3_n * v).normalize() // WARNING: Be careful with interpolation order!
+                
+                if self.is_smooth {
+                    let v1_n = vertex_cache.vertex_normals[self.indices[0]];
+                    let v2_n = vertex_cache.vertex_normals[self.indices[1]];
+                    let v3_n = vertex_cache.vertex_normals[self.indices[2]];
+                    let w = 1. - u - v;
+                    (v1_n * w + v2_n * u + v3_n * v).normalize() // WARNING: Be careful with interpolation order!
+                }
+                else {
+                    if self.normal.norm_squared() > 0.0 {
+                        self.normal
+                    } else {
+                        let verts = &vertex_cache.vertex_data;
+                        let [a, b, c] = self.indices.map(|i| verts[i]);
+                        get_tri_normal(&a, &b, &c)
                     }
-                    else {
-                        // If triangle has a stored normal (from mesh creation) use it,
-                        // otherwise compute face normal from vertex positions as fallback.
-                        if self.normal.norm_squared() > 0.0 {
-                            self.normal
-                        } else {
-                            let verts = &vertex_cache.vertex_data;
-                            let [a, b, c] = self.indices.map(|i| verts[i]);
-                            get_tri_normal(&a, &b, &c)
-                        }
-                        //cache.triangle_normal(self._id)
-                    }
-                //} 
-                //else {
-                //    info!("No cache provided, using flat shading by default...");
-                //    let [v1, v2, v3] = self.indices.map(|i| verts[i]);
-                //    get_tri_normal(&v1, &v2, &v3)
-                //}
+                }
             };
            
             let front_face = ray.is_front_face(tri_normal);
             let normal = if front_face { tri_normal } else { -tri_normal };
-            Some(HitRecord::new(p, normal, t, self.material_idx, front_face)) 
+            Some(HitRecord::new(ray.origin, p, normal, t, self.material_idx, front_face)) 
         }
         else {
             None
         }
         
     }
-}
-
-
-//fn get_beta_gamma_t(a: Vector3, b: Vector3, c: Vector3, o: Vector3, d: Vector3) -> (Float, Float, Float) { 
-//    // Helper function for computations in Slides 01_B, p.30
-//    // a, b, c are triangle corners
-//    // o, d are ray's origin and direction r(t) = o + d * t
-//    //
-//    // TODO: reduce verbosity and unnecessary operations
-//    // I've written in naive way for the beginning
-//    let (ax, ay, az) = (a[0], a[1], a[2]);
-//    let (bx, by, bz) = (b[0], b[1], b[2]);
-//    let (cx, cy, cz) = (c[0], c[1], c[2]);
-//    let (ox, oy, oz) = (o[0], o[1], o[2]);
-//    let (dx, dy, dz) = (d[0], d[1], d[2]);
-//
-//    // Construct A
-//    let A_x = Vector3::new(ax - bx, ay - by, az - bz);
-//    let A_y = Vector3::new(ax - cx, ay - cy, az - cz);
-//    let A_z = Vector3::new(dx, dy, dz);
-//    let A = Matrix3::from_cols(A_x, A_y, A_z);
-//    let A_determinant = A.determinant();
-//
-//    // Construct beta 
-//    let beta_x = Vector3::new(ax - ox, ay - oy, az - oz);
-//    let beta_y = Vector3::new(ax - cx, ay - cy, az - cz);
-//    let beta_z = Vector3::new(dx, dy, dx);
-//    let beta_matrix = Matrix3::from_cols(beta_x, beta_y, beta_z);
-//    let beta = beta_matrix.determinant() / A_determinant;
-//
-//    // Construct gamma
-//    let gamma_x = Vector3::new(ax - bx, ay - by, az - bz);
-//    let gamma_y = Vector3::new(ax - ox, ay - oy, az - oz);
-//    let gamma_z = Vector3::new(dx, dy, dz);
-//    let gamma_matrix = Matrix3::from_cols(gamma_x, gamma_y, gamma_z);
-//    let gamma = gamma_matrix.determinant() / A_determinant;
-//
-//    let t_x = Vector3::new(ax - bx, ay - by, az - bz);
-//    let t_y = Vector3::new(ax - cx, ay - cy, az - cz);
-//    let t_z = Vector3::new(ax - ox, ay - oy, az - oz);
-//    let t_matrix = Matrix3::from_cols(t_x, t_y, t_z);
-//    let t = t_matrix.determinant() / A_determinant;
-//
-//    (beta, gamma, t)
-//}
-
-//fn lengthy_but_simple_intersection(ray: &Ray, t_interval: &Interval, tri_indices: [usize; 3], verts: &VertexData) -> Option<(Vector3, Float)> {
-//    // Slides 01_B, p.14
-//    //
-//    //  n    a  
-//    //   \  / \
-//    //     /   \
-//    //   b ----- c
-//    let [a, b, c] = tri_indices.map(|i| verts[i]);
-//    let (beta, gamma, t) = get_beta_gamma_t(a, b, c, ray.origin, ray.direction);
-//
-//    // Conditions at p.32
-//    if !t_interval.contains(t) {
-//        return None;
-//    }
-//    if (beta + gamma) > 1. {
-//        return None;
-//    }
-//    if (0. > beta) || (0. > gamma) {
-//        return None;
-//    }
-//
-//    // Construct p from barycentric coords
-//    let p = ray.at(t);
-//    //let p = a + (beta * (b - a)) + (gamma * (c - a)); // p.27
-//    //assert!(approx_zero((p-pt).norm())); TODO: Why does it fail?
-//
-//    // Check for edge BA 
-//    let edge_ba = a - b;
-//    let edge_bc = c - b;
-//    let n = (edge_bc).cross(edge_ba); // vc in p.16
-//    let vp = (p - b).cross(edge_ba); // TODO: we can use the same vp for other checks, right?
-//    if vp.dot(n) <= 0.0 {
-//        return None;
-//    }
-//
-//    // Check for AC
-//    let edge_ca = a - c;
-//    let edge_ac = c - a;
-//    let vb = (edge_bc).cross(edge_ac);
-//    if vb.dot(n) <= 0.0 {
-//        return None;
-//    }
-//
-//    // Check for CB
-//    let edge_cb = b - c;
-//    let va = (edge_ca).cross(edge_cb);
-//    if va.dot(n) <= 0.0 {
-//        return None;
-//    }
-//
-//    Some((p, t))
-//}
-
-fn moller_trumbore_intersection(ray: &Ray, t_interval: &Interval, tri_indices: [usize; 3], verts: &VertexData) -> Option<(Float, Float, Float)> {
-    // Based on Möller-Trumbore algorithm
-        //
-        //     a (pivot)
-        //    / \
-        //  b  -  c
-        // 
-        // WARNING: Assumes given interval has incorporated relevant epsilon e.g.
-        // instead of [0.0, inf], [0.0001, inf] is given otherwise there might be
-        // floating point errors.
-        // TODO: Is there something wrong in this function?
-        let tri_coords = tri_indices.map(|i| verts[i]);
-        let [tri_pivot, tri_left, tri_right] = tri_coords;        
-        let edge_ab = tri_left - tri_pivot;
-        let edge_ac = tri_right - tri_pivot;
-
-        // Scalar triple product https://youtu.be/fK1RPmF_zjQ
-        debug_assert!(ray.direction.is_normalized());
-        let perp = ray.direction.cross(edge_ac);
-        let determinant: Float = perp.dot(edge_ab);
-        if (determinant > -t_interval.min) && (determinant < t_interval.min) {
-            return None;
-        }
-
-        let inverse_determinant = 1.0 as Float / determinant;
-        let dist = ray.origin - tri_pivot;
-
-        let barycentric_u = dist.dot(perp) * inverse_determinant;
-        if (barycentric_u < 0.0) || (barycentric_u > 1.0) {
-            return None;
-        }
-
-        let another_perp = dist.cross(edge_ab);
-        let barycentric_v = ray.direction.dot(another_perp) * inverse_determinant;
-        if (barycentric_v < 0.0) || ((barycentric_u + barycentric_v) > 1.0) {
-            return None;
-        }
-
-        // Get ray t
-        let t = edge_ac.dot(another_perp) * inverse_determinant;
-        if !t_interval.contains(t) {
-            return None;
-        }
-
-        Some((barycentric_u, barycentric_v, t))
 }
 
 #[derive(Debug, Deserialize, Clone, Default)]
@@ -400,7 +246,6 @@ impl PrimitiveShape for Plane {
         if t_interval.contains(t) {
             // Construct Hit Record
             let front_face = ray.is_front_face(self.normal);
-
             let normal = if front_face { self.normal } else { -self.normal };
             Some(HitRecord::new(ray.at(t), normal, t, self.material_idx, front_face))
         }
