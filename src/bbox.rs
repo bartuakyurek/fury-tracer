7fe73c63617e40081d795cf79fe164510d5dd4cf
bartuakyurek <77360680+bartuakyurek@users.noreply.github.com>
2025-11-10 14:54:56 +0300

    Introduce BVH tree (!)
    There is something wrong here. Compiler and clippy highlights default( )
    but it works outside of match expression.

--- src/bbox.rs
+++ src/bbox.rs
@@ -1,7 +1,8 @@
 /*
 
-    Axis Aligned Bounding Box
+    Axis Aligned Bounding Box and Bounding Volume Hierarchy.
 
+    
     @author: bartu
     @date: 9 Nov, 2025
 */
@@ -9,8 +10,10 @@
 
 use crate::prelude::*;
 
-use crate::interval::{Interval};
+use crate::interval::{FloatConst, Interval};
 use crate::json_structs::{VertexData};
+use crate::ray::{Ray, HitRecord};
+use crate::shapes::Shape;
 
 pub struct BBox {
     xmin: Float, xmax: Float,
@@ -38,8 +41,62 @@ impl BBox {
             depth: zint.max - zint.min,
         }
     }
+
+    fn intersect(&self, ray: &Ray) -> bool {
+        todo!()
+    }
 }
 
 pub trait BBoxable {
     fn get_bbox(&self, verts: &VertexData) -> BBox;
+}
+
+// ====================================================================================================
+// Bounding Volume Hierarchy
+// ====================================================================================================
+// Intersection logic follows Slides 03_acceleration_structures p.64
+// and the binary tree creation is inspired by:
+// https://google.github.io/comprehensive-rust/smart-pointers/exercise.html
+
+
+struct Subtree<T>(Option<Arc<Node<T>>>); // Inspired by the binary tree exercise (see link in header)
+
+struct Node<T> {
+    value: T,
+    left: Subtree<T>,
+    right: Subtree<T>,
+}
+
+pub type BVHSubtree = Subtree<BBox>;
+pub type BVHNode = Node<BBox>;
+
+impl BVHSubtree {
+    pub fn intersect(&self, ray: &Ray, rec: &mut HitRecord) -> bool {
+        
+        match &self.0 {
+            None => { return false; }
+            Some(node) => {
+                
+                if node.value.intersect(ray) {
+                    let mut rec1: HitRecord::default();
+                    let mut rec2: HitRecord::default();
+
+                    rec.ray_t = FloatConst::INF;
+
+                    let hitleft: bool = node.left.intersect(ray, &mut rec1);
+                    let hitright: bool = node.right.intersect(ray, &mut rec2);
+
+                    if hitleft { rec = rec1; }
+                    if hitright { rec = rec2; }
+                    return hitleft || hitright;
+                }
+                else {
+                    // The ray entirely misses this bounding box
+                    return false;
+                }
+            }
+        }
+
+        todo!()
+    }
 }
