6a6d074a11612f36f879ed2bbd05d51cf21e66b5
bartuakyurek <77360680+bartuakyurek@users.noreply.github.com>
2025-11-12 16:51:44 +0300

    Clean previous intersect( )  (!)
    It'll be added back soon, intersect( ) was not compatible with current
    signatures (i.e. hitrecord is not updated anywhere in the recursion so
    that needs to be fixed first) also wanted to experiment with
     Shape + BBoxable trait bounds in build( )

--- src/acceleration.rs
+++ src/acceleration.rs
@@ -2,9 +2,10 @@
 
 
 use crate::prelude::*;
-use crate::shapes::Shape;
+use crate::json_structs::VertexData;
+use crate::shapes::{Shape, HeapAllocatedShape};
 use crate::ray::{Ray, HitRecord};
-use crate::bbox::BBox;
+use crate::bbox::{BBox, BBoxable};
 
 
 // ====================================================================================================
@@ -14,60 +15,37 @@ use crate::bbox::BBox;
 // https://google.github.io/comprehensive-rust/smart-pointers/exercise.html
 
 
-struct Subtree<T>(Option<Arc<Node<T>>>); // Inspired by the binary tree exercise (see link in header)
-
-struct Node<T> {
-    value: T,
-    left: Subtree<T>,
-    right: Subtree<T>,
+/// BVH node storing a bounding box, optional children, and a list of objects for leaves.
+#[derive(Debug)]
+pub struct BVHNode {
+    pub bbox: BBox,
+    pub left: Option<Arc<BVHNode>>,
+    pub right: Option<Arc<BVHNode>>,
+    pub objects: Vec<HeapAllocatedShape>,
 }
 
-pub type HeapBBoxable = Arc<dyn BBoxable>;
-pub type BVHSubtree = Subtree<HeapBBoxable>;
-pub type BVHNode = Node<HeapBBoxable>;
+/// BVHSubtree is a wrapper around an optional root node.
+#[derive(Debug, Clone)]
+pub struct BVHSubtree(pub Option<Arc<BVHNode>>);
 
 impl BVHSubtree {
-
-    /// Construct BVH binary tree given a list of bounding-boxable
-    // NOTE: BBoxable has get_bbox( ) though I'm not sure why I wanted use
-    // generics here, instead of plain Vec<BBox>. Perhaps it'd be useful for
-    // other non-AABB bounding boxes yet it's just an overkill at the moment?
-    // just doing it for the sake of Rust practice
-    // Actually it is useful for Scene to not call .getbbox( ) rather it just
-    // holds scene objects in which some are bboxable, making this function flexible.
-    pub fn build<T>(bbox_list: &Vec<T>) -> Self 
-    where 
-        T: BBoxable,
+    /// Build a BVH from a list of shapes using their bounding boxes.
+    /// verts needed for get_bbox( ) called inside, since shapes only store indices, 
+    /// not the actual verts. 
+    pub fn build<T>(shapes: &Vec<T>, verts: &VertexData) -> Self 
+        where 
+            T: Shape + BBoxable,
     {
-        todo!()
-    }
-
-    pub fn intersect(&self, ray: &Ray, rec: &mut HitRecord) -> bool {
-        // Refers Slides 03_acceleration_structures p.64
-        match &self.0 {
-            None => { return false; }
-            Some(node) => {
-                
-                if node.value.intersect(ray) {
-                    
-                    let mut rec1 = HitRecord::default();
-                    let mut rec2 = HitRecord::default();
-
-                    rec.ray_t = FloatConst::INF;
-
-                    let hitleft: bool = node.left.intersect(ray, &mut rec1);
-                    let hitright: bool = node.right.intersect(ray, &mut rec2);
-
-                    if hitleft { *rec = rec1; }
-                    if hitright { *rec = rec2; }
-                    return hitleft || hitright;
-                }
-                else {
-                    // The ray entirely misses this bounding box
-                    return false;
-                }
-            }
+        
+        if shapes.is_empty() {
+            return BVHSubtree(None);
         }
 
+    }
+
+    /// Intersect a ray with the BVH. Returns true if any hit was found and fills `rec` with the closest hit.
+    pub fn intersect(&self, ray: &Ray, t_interval: &Interval, vertex_cache: &crate::scene::HeapAllocatedVerts, rec: &mut HitRecord) -> bool {
+        
+        false
     }
 }
