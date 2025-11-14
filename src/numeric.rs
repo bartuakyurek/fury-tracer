e5e52e3ee77c277827d6babdb6ace03c8e29142c
bartuakyurek <77360680+bartuakyurek@users.noreply.github.com>
2025-11-14 15:42:45 +0300

    add transform_point( )

--- src/numeric.rs
+++ src/numeric.rs
@@ -14,12 +14,13 @@
     @author: Bartu
 */
 
-use bevy_math::{DMat3, DVec3, DMat4};
+use bevy_math::{DMat3, DMat4, DVec3, DVec4};
 pub type Int = i32;
 pub type Float = f64; // WARNING: If you want to change it to f32, don't forget to update Vector3 and Matrix3 types
 pub type Vector3 = DVec3; 
 pub type Matrix3 = DMat3;
 pub type Matrix4 = DMat4;
+pub type Vector4 = DVec4;
 
 //#[derive(Clone, Copy, Debug, Default)]
 //pub struct Vector3(pub DVec3); // To declare a type and use impl traits on this type
@@ -28,4 +29,10 @@ pub type Matrix4 = DMat4;
 
 pub fn approx_zero(x: Float) -> bool {
     x.abs() < 1e-8
+}
+
+pub fn transform_point(mat: &Matrix4, v: &Vector3) -> Vector3 {
+    let v4 = Vector4::new(v.x, v.y, v.z, 1.0);
+    let r = *mat * v4;
+    Vector3::new(r.x, r.y, r.z)
 }
