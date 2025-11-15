0ee9cc612657d87bb42253f32f44f3ba6c338556
bartuakyurek <77360680+bartuakyurek@users.noreply.github.com>
2025-11-15 11:13:20 +0300

    add comments

--- src/ray.rs
+++ src/ray.rs
@@ -47,9 +47,13 @@ impl Ray {
 
     #[inline]
     pub fn inverse_transform(&self, inv_matrix: &Matrix4) -> Ray {
+        // slides 04, p.50
         let local_origin = transform_point(inv_matrix, &self.origin);
-        let mat3 = Matrix3::from_mat4(*inv_matrix);
-        let local_direction = (mat3 * self.direction).normalize();
+        let mat3 = Matrix3::from_mat4(*inv_matrix); //.transpose();
+        let mut local_direction = mat3 * self.direction;
+        local_direction = local_direction.normalize(); // slides 04, p.53
+        //let local_direction = transform_point(inv_matrix, &self.direction);
+        debug_assert!(local_direction.is_normalized());
         Ray::new(local_origin, local_direction)
     }
 }
