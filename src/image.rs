1ad491819d02b4d0a4cf75bef053b9573110e0a2
bartuakyurek <77360680+bartuakyurek@users.noreply.github.com>
2025-12-21 22:15:34 +0300

    fix normalization (!?)
    not really a fix...  i've realized the reason some parts appear all white is not
    due to texture coordinates but  it was a radiance issue. this logic works better
    but it is only a workaround and some scenes now appear darker (but at
    least we can see the texture...)

--- src/image.rs
+++ src/image.rs
@@ -162,7 +162,12 @@ impl Textures {
                 let (col, row) = (uv[0] * image.width as Float, uv[1] * image.height as Float); // image coordinate (see slides 06, p.8)
                 let color = image.interpolate(row, col, interpolation);
                 if apply_normalization {
-                    color / image_texmap.normalizer // By default divide by 255
+                    if image_texmap.normalizer > 1. {
+                        color / image_texmap.normalizer
+                    } else {
+                        color / 255.
+                    }
+                    //color / 255.  // By default divide by 255
                 } else {
                     color
                 }
