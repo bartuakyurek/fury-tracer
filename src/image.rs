46ee4bb5be71699dd16b8ba29bd68d11c02e8f06
bartuakyurek <77360680+bartuakyurek@users.noreply.github.com>
2025-12-21 23:21:25 +0300

    add note

--- src/image.rs
+++ src/image.rs
@@ -162,7 +162,7 @@ impl Textures {
                 let (col, row) = (uv[0] * image.width as Float, uv[1] * image.height as Float); // image coordinate (see slides 06, p.8)
                 let color = image.interpolate(row, col, interpolation);
                 if apply_normalization {
-                    if image_texmap.normalizer > 1. {
+                    if image_texmap.normalizer > 1. { // TODO: This isn't a real solution but it avoids exploded radiance in some cases (but makes the image appear darker, so it's not really solving the real issue)
                         color / image_texmap.normalizer
                     } else {
                         color / 255.
