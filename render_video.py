5aec172531a204bdcf7354f5a6bb46270b07b9cf
bartuakyurek <77360680+bartuakyurek@users.noreply.github.com>
2026-01-01 22:21:14 +0300

    update for hw4 video

--- render_video.py
+++ render_video.py
@@ -3,14 +3,14 @@ import os
 
 # Folder containing your PNG frames
 #jsonname = "tap"
-modelname = "tap"
+modelname = "tunnel_of_doom"
 #output_path = "./outputs/hw3/tap_water/json/"
 
-frames_dir = "./outputs/hw3/tap_water/json/" # os.path.join(output_path, jsonname) 
+frames_dir = "./outputs/hw4/tunnel_of_doom/" # os.path.join(output_path, jsonname) 
 output_video = f"{modelname}.mp4"
 
 # Frame name like tap_0000.png
-frame_pattern = f"{modelname}_%04d.png"  
+frame_pattern = f"{modelname}_%03d.png"  
 
 subprocess.run([
     "ffmpeg",
